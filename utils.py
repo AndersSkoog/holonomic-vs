4ea1bfc2fbdff73987b9e115789c7fb1a0d35ee1
anders <anders.skooog@gmail.com>
2026-02-04 21:39:25 +0100

    "some changes"

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent
 def proj_file_path(path:str): return str(BASE_DIR) + path
 
 def read_file(filepath:str) -> str:
-  f = open(proj_file_path(filepath))
+  f = open(filepath)
   txt = f.read()
   f.close()
   return txt
