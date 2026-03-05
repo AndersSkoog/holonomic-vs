15568773398b1561bada1e17b44e6579311ffcb5
anders <anders.skooog@gmail.com>
2026-03-05 20:11:32 +0100

    "some changes"

--- SO.py
+++ SO.py
@@ -126,6 +126,56 @@ def torsion_curve_arc(pos_start, frame_start, pos_end, frame_end,
 
     return np.array(pts)
 
+
+import numpy as np
+
+
+def torsion_curve(pos_start, frame_start, pos_end, frame_end, t_value, helix_radius=0.1, axis_index=2, num_points=50):
+    """
+    Compute points along the torsion curve induced by SLERP between two frames.
+
+    Parameters
+    ----------
+    pos_start : array_like, shape (3,)
+        3D start position.
+    frame_start : array_like, shape (3,3)
+        3x3 rotation matrix at start position (SO(3) frame).
+    pos_end : array_like, shape (3,)
+        3D end position.
+    frame_end : array_like, shape (3,3)
+        3x3 rotation matrix at end position (SO(3) frame).
+    t_value : float
+        Interpolation factor between 0 and 1.
+    helix_radius : float
+        Distance along chosen axis from the interpolated position.
+    axis_index : int
+        Which axis of the frame to trace: 0=x,1=y,2=z (default z).
+    num_points : int
+        Number of points along the curve to generate.
+
+    Returns
+    -------
+    curve_points : np.ndarray, shape (num_points, 3)
+        3D points tracing the torsion curve.
+    """
+    pos_start = np.asarray(pos_start, dtype=float)
+    pos_end = np.asarray(pos_end, dtype=float)
+    curve_points = []
+
+    # Interpolation along the segment
+    for i in range(num_points):
+        t = t_value * (i / (num_points - 1))
+        pos_t = pos_start + (pos_end - pos_start) * t
+        # SLERP the frame at this fraction
+        R_t = slerp(frame_start, frame_end, t)
+        # Offset along the chosen frame axis
+        offset = R_t[:, axis_index] * helix_radius
+        curve_points.append(pos_t + offset)
+
+    return np.array(curve_points)
+
+
+
 # Slerp Demo
 if __name__ == "__main__":
   from math import tau, sqrt
@@ -168,13 +218,22 @@ if __name__ == "__main__":
       tor_res = 50
       tor_plane = [(0,1),(0,2),(1,2)][params["tor_axis"]]
       tor_col =  ["red","green","blue"][params["tor_axis"]]
-      tc_x,tc_y,tc_z = zip(*torsion_curve_arc(pos1,R1,pos2,R2,t,tor_plane,0.3,tor_res,tor_res))
+      #tca_x,tca_y,tca_z = zip(*torsion_curve_arc(pos1,R1,pos2,R2,t,tor_plane,0.3,tor_res,tor_res))
+      tc0_x,tc0_y,tc0_z = zip(*torsion_curve(pos1,R1,pos2,R2,t,0.15,0,tor_res))
+      tc1_x,tc1_y,tc1_z = zip(*torsion_curve(pos1,R1,pos2,R2,t,0.15,1,tor_res))
+      tc2_x,tc2_y,tc2_z = zip(*torsion_curve(pos1,R1,pos2,R2,t,0.15,2,tor_res))
+
+
       #tc2_x, tc2_y, tc2_z = zip(*torsion_curve_arc(pos1, R1, pos2, R2, t, (0,2), 0.3, tc_res, tc_res))
       #tc3_x, tc3_y, tc3_z = zip(*torsion_curve_arc(pos1, R1, pos2, R2, t, (1,2), 0.3, tc_res, tc_res))
       ax.text(params["x1"] + 0.3,params["y1"],params["z1"],"start_pos")
       ax.text(params["x2"] + 0.3,params["y2"],params["z2"],"end_pos")
       ax.text(pos_t[0] + 0.3,pos_t[1],pos_t[2],"t_pos")
-      ax.plot(xs=tc_x,ys=tc_y,zs=tc_z,lw=0.3,color=tor_col)
+      #ax.plot(xs=tca_x,ys=tca_y,zs=tca_z,lw=0.3,color=tor_col)
+      ax.plot(xs=tc0_x,ys=tc0_y,zs=tc0_z,lw=1.0,color="red")
+      ax.plot(xs=tc1_x,ys=tc1_y,zs=tc1_z,lw=1.0,color="green")
+      ax.plot(xs=tc2_x,ys=tc2_y,zs=tc2_z,lw=1.0,color="blue")
+
       ax.plot(xs=[pos1[0],pos_t[0]],ys=[pos1[1],pos_t[2]],zs=[pos1[2],pos_t[2]],lw=1.0,color="purple")
       #ax.plot(xs=tc2_x,ys=tc2_y,zs=tc2_z,lw=0.3,color="green")
       #ax.plot(xs=tc3_x,ys=tc3_y,zs=tc3_z,lw=0.3,color="blue")
